7307b6eb0296238ea5691f9735784466dcaf551a
achmadrivaldi <mrtampan54@gmail.com>
2022-12-05 00:39:03 +0700

    ternyata render awal dulu divercel

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://nextjsphoto.vercel.app';
+  : 'https://mrtampan.github.io/nextphotodata';
